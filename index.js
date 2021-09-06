939b85919f4e73a4bbdf7410fd68ff2bf5c00ab0
yuler <joyful4yu@gmail.com>
2021-09-07 00:57:00 +0800

    fix: add index.js & console warns

---
+++ index.js
@@ -0,0 +1,8 @@
+console.warn(
+	'This package is collection some GitHub Actions. No export of anything.',
+)
+console.warn('Publish to npm is only for GitHub action test.')
+console.warn('For the usage, please read the README.')
+
+// noop
+export default function () {}
